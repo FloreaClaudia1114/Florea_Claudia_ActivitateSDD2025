8626c3929d2104d02cddafd4d5a8f42de01069af
claud <claud@CLAUDIA>
2025-05-08 22:24:28 +0300

    taskurisuplimentare HT partea 2

--- taskurisuplimentare_hashtable.c
+++ taskurisuplimentare_hashtable.c
@@ -1,72 +1,125 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <malloc.h>
 
+// Structura Cladire
 typedef struct Cladire {
-	int id;
-	int an_construire;
-	char* denumire;
-	unsigned char categorie_risc;
+    int id;
+    int an_construire;
+    char* denumire;
+    unsigned char categorie_risc;
 } Cladire;
 
+// Structura Nod
 typedef struct Nod {
-	Cladire cladire;
-	Nod* next;
+    Cladire cladire;
+    struct Nod* next;
 } Nod;
 
-//creare structura tabela HT, aceasta contine un vector de pointeri la nod care sa retina nodurile si aceasta dimensiune
+// Structura Hash Table (HT)
 typedef struct HT {
-	int dim;
-	Nod** tabela;
+    int dim;
+    Nod** tabela;
+} HT;
 
-}HT;
+// Funcție de citire Cladire din fișier
 Cladire citirecladiredinfisier(FILE* file) {
-	Cladire c;
-	char buffer[100];
-	char sep[4] = ".;\n";
-	fgets(buffer, 100, file);
-	c.id = atoi(strtok(buffer, sep));
-	c.an_construire = atoi(strtok(NULL, sep));
+    Cladire c;
+    char buffer[100];
+    char sep[4] = ".;\n";
+    fgets(buffer, 100, file);
+    c.id = atoi(strtok(buffer, sep));
+    c.an_construire = atoi(strtok(NULL, sep));
 
-	char* aux;
-	aux = strtok(NULL, sep);
-	c.denumire = (char*)malloc(strlen(aux) + 1);//alocam memorie
-	strcpy(c.denumire, aux);//destinatia si sursa
+    char* aux;
+    aux = strtok(NULL, sep);
+    c.denumire = (char*)malloc(strlen(aux) + 1);
+    strcpy(c.denumire, aux);
 
-	c.categorie_risc = strtok(NULL, sep)[0];//il indexam luam un singur element
-	return c;
-
+    c.categorie_risc = strtok(NULL, sep)[0];
+    return c;
 }
 
-//functia de afisare a unei cladire  
-//%c - un singur caracter, %s un sir de caractere pt pointer
+// Afișare Cladire
 void afisarecladire(Cladire cladire) {
-	printf("Id: %d\n", cladire.id);
-	printf("An_construire: %d\n", cladire.an_construire);
-	printf("Denumire: %s\n", cladire.denumire);
-	printf("Categorie_risc: %c\n", cladire.categorie_risc);
+    printf("Id: %d\n", cladire.id);
+    printf("An_construire: %d\n", cladire.an_construire);
+    printf("Denumire: %s\n", cladire.denumire);
+    printf("Categorie_risc: %c\n\n", cladire.categorie_risc);
 }
 
-void afisarelistacladiri(Nod* cap) {
-	while (cap) {
-		afisarecladire(cap->cladire);
-		cap = cap->next;
-	}
+// Inserare Cladire în lista
+void cladirenouainlista(Nod** cap, Cladire cladireNoua) {
+    Nod* nou = (Nod*)malloc(sizeof(Nod));
+    nou->cladire = cladireNoua;
+    nou->next = NULL;
+
+    if (*cap == NULL) {
+        *cap = nou;
+    }
+    else {
+        Nod* p = *cap;
+        while (p->next) {
+            p = p->next;
+        }
+        p->next = nou;
+    }
 }
 
-void cladirenouainlista(Nod* cap, Cladire cladireNoua) {
-	Nod* p = cap;//ne deplasam pn la ultimul nod 
-	while (p->next) {
-		p = p->next;
-	}
-	Nod* nou = (Nod*)malloc(sizeof(Nod));
-	nou->cladire = cladireNoua;//shallow copy
-	nou->next = NULL;
-	p->next = nou;
+// Inițializare Hash Table
+HT* initializareHashTable(int dimensiune) {
+    HT* ht = (HT*)malloc(sizeof(HT));
+    ht->dim = dimensiune;
+    ht->tabela = (Nod**)malloc(dimensiune * sizeof(Nod*));
+    for (int i = 0; i < dimensiune; i++) {
+        ht->tabela[i] = NULL;
+    }
+    return ht;
 }
 
-HT initializareHT(int dimensiune) {
+// Funcție de hash (anul construirii % dimensiune)
+int hash(HT* ht, int an_construire) {
+    return an_construire % ht->dim;
+}
 
+// Adăugare Cladire în Hash Table
+void adaugaInHT(HT* ht, Cladire cladire) {
+    int index = hash(ht, cladire.an_construire);
+    cladirenouainlista(&(ht->tabela[index]), cladire);
+}
+
+// Afișare Hash Table
+void afisareHT(HT* ht) {
+    for (int i = 0; i < ht->dim; i++) {
+        printf("Index %d:\n", i);
+        Nod* p = ht->tabela[i];
+        while (p) {
+            afisarecladire(p->cladire);
+            p = p->next;
+        }
+    }
+}
+
+// Main
+int main() {
+    HT* hashtable = initializareHashTable(7);
+
+    FILE* file = fopen("cladiri.txt", "r");
+    if (!file) {
+        printf("Eroare la deschiderea fișierului!\n");
+        return 1;
+    }
+
+    while (!feof(file)) {
+        Cladire c = citirecladiredinfisier(file);
+        adaugaInHT(hashtable, c);
+    }
+
+    fclose(file);
+
+    afisareHT(hashtable);
+
+    return 0;
 }
